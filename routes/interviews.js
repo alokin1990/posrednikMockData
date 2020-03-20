ac8ce7e7cc792540d750d5091f86997e690ec7c1
alokin1337 <nefremovski@gmail.com>
2020-03-20 14:38:14 +0100

    remove interviews

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,17 +1,18 @@
 module.exports = {
-  jobID: "jobIDSN1000 ",
-  blocked: false,
-  jobTitle: "Kitchen Worker",
-  companyID: "comIDSN3959593495",
-  companyPersonName: "Mark",
-  companyPersonLstName: "Trenz",
-  userID: "usrIDSN2895864",
-  userFirstName: "Nikola",
-  userLastName: "Efremovski",
-  msgs: [
-    {
-      user: "userFirstName userLastName",
-      msg: ""
-    }
-  ]
+  //we wont have interviews
+  // jobID: "jobIDSN1000 ",
+  // blocked: false,
+  // jobTitle: "Kitchen Worker",
+  // companyID: "comIDSN3959593495",
+  // companyPersonName: "Mark",
+  // companyPersonLstName: "Trenz",
+  // userID: "usrIDSN2895864",
+  // userFirstName: "Nikola",
+  // userLastName: "Efremovski",
+  // msgs: [
+  //   {
+  //     user: "userFirstName userLastName",
+  //     msg: ""
+  //   }
+  // ]
 };
